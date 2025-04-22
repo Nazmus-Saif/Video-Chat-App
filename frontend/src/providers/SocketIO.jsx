164ce6482be69b8beac6fe3371b0f34fb27c1008
Nazmus Saif <124804475+Nazmus-Saif@users.noreply.github.com>
2025-04-22 19:00:09 +0600

    Update SocketIO.jsx

--- frontend/src/providers/SocketIO.jsx
+++ frontend/src/providers/SocketIO.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://video-chat-app-backend.vercel.app"), []);
+  const socket = useMemo(() => io("https://video-chat-app-backend-phi.vercel.app"), []);
 
   return (
     <SocketContext.Provider value={{ socket }}>
